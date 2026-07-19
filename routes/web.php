a8adcda5b61563f42d2f3f1489a36aa5ccbf962c
Gumilar L Wijayadi <gumilarlesmana@gmail.com>
2026-07-19 15:10:46 +0700

    feat: update routes web

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,18 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WebController;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 Route::get('/', [WebController::class, 'home'])->name('home');
-Route::get('/blog/{slug}', [WebController::class, 'blog'])->name('blog');
-Route::get('/service/{slug}', [WebController::class, 'service'])->name('service');
+// Blog Page
+Route::get('/blog', [WebController::class, 'blog'])->name('blog');
+Route::get('/blog/{slug}', [WebController::class, 'blogDetail'])->name('blog.detail');
+
+// Service Page
+Route::get('/service', [WebController::class, 'service'])->name('service');
+Route::get('/service/{slug}', [WebController::class, 'serviceDetail'])->name('service.detail');
+
+// About Page
 Route::get('/about', [WebController::class, 'about'])->name('about');
 
-// Contact us
+// Contact us Page
 Route::get('/contact', [WebController::class, 'contact'])->name('contact');
 Route::post('/contact/save', [WebController::class, 'contactHandle'])->name('contact.save');
