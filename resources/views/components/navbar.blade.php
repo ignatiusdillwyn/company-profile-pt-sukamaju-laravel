374ce85e9fa9d31b8c11045c6a22b4f56e732b51
Gumilar L Wijayadi <gumilarlesmana@gmail.com>
2026-07-25 11:05:24 +0700

    feat: path using routing name

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -13,10 +13,10 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
-                        <a class="nav-link active" href="#home">Home</a>
+                        <a class="nav-link active" href="{{ route('home') }}">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/service">Service</a>
+                        <a class="nav-link" href="{{ route('service') }}">Service</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="/blog">Blog</a>
