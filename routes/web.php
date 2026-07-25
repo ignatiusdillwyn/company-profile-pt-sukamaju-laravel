54aacc946055cdba57a7c1f012fa14173addc3f3
ignatius_dillwyn <v.ignatius.dillwyn@adira.co.id>
2026-07-25 15:55:06 +0700

    remove /home

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\WebController;
 use App\Http\Controllers\AuthController;    
 use App\Http\Controllers\Admin\DashboardController;
 
-Route::get('/home', [WebController::class, 'home'])->name('home');
+Route::get('/', [WebController::class, 'home'])->name('home');
 
 // Blog Page
 Route::get('/blog', [WebController::class, 'blog'])->name('blog');
