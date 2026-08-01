7529ef461c576cce9bb7f2a4f0595c37d3bdbeec
Gumilar L Wijayadi <gumilarlesmana@gmail.com>
2026-08-01 14:34:31 +0700

    feat: update seeder content article model and seeder

--- app/Models/ContentModel.php
+++ app/Models/ContentModel.php
@@ -3,24 +3,40 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class ContentModel extends Model
 {
-  public function create(
-    $title,
-    $slug,
-    $content,
-    $author,
-    $created_at,
-    $updated_at
-  ) {
-    return $this->insert([
-      'title' => $title,
-      'slug' => $slug,
-      'content' => $content,
-      'author' => $author,
-      'created_at' => $created_at,
-      'updated_at' => $updated_at
-    ]);
+  public function create($content) 
+  {
+    foreach ($content as $item) {
+      
+      $userId       = $item['user_id'];
+      $articleType  = $item['article_type'];
+      $title        = $item['title'];
+      $slug         = $item['slug'];
+      $image        = $item['image'];
+      $content      = $item['content'];
+      $isPublished  = $item['is_published'];
+      $createdAt    = $item['created_at'];
+      $updatedAt    = $item['updated_at'];
+
+      // Simpan data ke database
+      $data = DB::table('table_articles')
+              ->insert([
+        'user_id'       => $userId,
+        'article_type'  => $articleType,
+        'title'         => $title,
+        'slug'          => $slug,
+        'image'         => $image,
+        'content'       => $content,
+        'is_published'  => $isPublished,
+        'created'    => $createdAt,
+        'updated'    => $updatedAt
+      ]);
+
+    }
+
+    return $data;
   }
 }
