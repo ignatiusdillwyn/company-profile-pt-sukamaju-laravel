8c86e598a36b3dad04f69bc0a57335c04bf915a4
Gumilar L Wijayadi <gumilarlesmana@gmail.com>
2026-08-08 10:52:29 +0700

    feat: adding email function

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -40,8 +40,38 @@ class WebController
     public function contactHandle(StoreFormRequest $request)
     {
         $validated = $request->validated();
+        // dd($validated);
         $this->contactModel->createContact($request);
-        Mail::to('ignadillwyn2@gmail.com')->send(new FormMail());
+
+        $data = [
+            'name' => $validated['full_name'],
+            'email' => $validated['email'],
+            'subject' => 'Contact Form Submission',
+            'notes' => $validated['notes'],
+        ];
+        
+        try {
+
+            Mail::to('ignadillwyn2@gmail.com')
+                ->cc('gumilarlesmana@gmail.com')
+                ->send(new FormMail($data));
+
+            dd([
+                'step' => 'AFTER SEND',
+                'message' => 'EMAIL SENT SUCCESSFULLY',
+            ]);
+
+        } catch (\Throwable $e) {
+
+            dd([
+                'step' => 'ERROR',
+                'message' => $e->getMessage(),
+                'file' => $e->getFile(),
+                'line' => $e->getLine(),
+                'trace' => $e->getTraceAsString(),
+            ]);
+        } 
+        
         return redirect()->route('contact')->with('success', 'Message Sent Successfully!');
     }
 
