10041018d89d46922563a82cdd5fab93bef66543
Gumilar L Wijayadi <gumilarlesmana@gmail.com>
2026-08-08 10:53:27 +0700

    feat: view blade rom email

---
+++ resources/views/emails/email.blade.php
@@ -0,0 +1,14 @@
+<p>
+  Thank you for your message. We will get back to you soon.
+</p>
+<ul>
+  <li>
+    <strong>Name:</strong> {{ $email['name'] }}
+  </li>
+  <li>
+    <strong>Email:</strong> {{ $email['email'] }}
+  </li>
+  <li>
+    <strong>Message:</strong> {{ $email['notes'] }}
+  </li>
+</ul>
